cc0998be8ce18e3258a1c209ec780444261c64f1
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2019-04-25 01:33:22 -0500

    most everything is working, still have some jquery bugs though

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Only the Provolonely');
+INSERT INTO burgers (name) VALUES ('Sweet Chili O Mine');
+INSERT INTO burgers (name) VALUES ("Chile Relleno-You-Didn't");
